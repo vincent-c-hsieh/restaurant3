89447b38bb7545fabd1498dfe1ec1876919087d4
Vincent Hsieh <vincent.chi.hsieh@gmail.com>
2019-12-25 13:27:41 -0500

    restaurant 3 finished

--- portfolio/scripts/script.js
+++ portfolio/scripts/script.js
@@ -1,1 +1,16 @@
-//first script//
+//form javascript validation//
+function validateForm() {
+  var x = document.forms["myForm"]["name"].value;
+  var y = document.forms ["myForm"]["email"].value;
+  var z = document.forms ["myForm"]["phone"].value;
+  if (x == "") {
+    alert("Must fill out your name");
+    return false;
+  } else if (y == "") {
+    alert ("must fill out your email");
+    return false;
+  } else if (z == ""){
+    alert ("must fill out your phone number");
+    return false;
+  }
+}
